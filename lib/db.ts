7518cc1a59ce1222e59954ba7218c120c1dc9420
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-05-11 08:49:53 +0530

    idk

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,10 @@
-import { PrismaClient } from "@/db/generated/prisma";
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = global as unknown as { 
+    prisma: PrismaClient
 }
 
+export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-export const prisma = global.prisma || new PrismaClient({
-  log: process.env.NODE_ENV === 'development' 
-    ? ['query', 'error', 'warn'] 
-    : ['error'],
-});
-
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
